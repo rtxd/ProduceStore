57df41702b668a4f10c88c110fcca6fe7bef64e1
Alejandro Munoz <alejandro.munoz@student.uts.edu.au>
2018-06-01 19:47:12 +1000

    Added configuration for local path |

--- UTS.ProduceStore.WebFrontEnd/Startup.cs
+++ UTS.ProduceStore.WebFrontEnd/Startup.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using System;
+using System.IO;
 
 [assembly: OwinStartupAttribute(typeof(UTS.ProduceStore.WebFrontEnd.Startup))]
 namespace UTS.ProduceStore.WebFrontEnd
@@ -8,6 +10,8 @@ namespace UTS.ProduceStore.WebFrontEnd
     {
         public void Configuration(IAppBuilder app)
         {
+            string path = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\Data"));
+            AppDomain.CurrentDomain.SetData("DataDirectory", path);
             ConfigureAuth(app);
         }
     }
