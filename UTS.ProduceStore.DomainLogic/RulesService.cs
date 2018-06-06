5b57137eacbf7c812beaa1ffb1f18a949c8bd707
maxwellwren11 <maxwell.prasad@student.uts.edu.au>
2018-06-06 19:24:29 +1000

    Complete editor methods in domain logic project

--- UTS.ProduceStore.DomainLogic/RulesService.cs
+++ UTS.ProduceStore.DomainLogic/RulesService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,19 +8,65 @@ using UTS.ProduceStore.Data;
 
 namespace UTS.ProduceStore.DomainLogic
 {
+    //Domain Code for Editor and Approver
     public class RulesService
     {
-        //Domain Code for Editor and Approver
         
-/*
+        //Generic methods
+
+        //Method for retrieving all rules with a given status
         public List<Rule> GetRulesByStatus(string status)
         {
             using (var db = new ProduceStoreEntities())
             {
-                
+                var query = from r in db.Rules
+                            where r.RuleStatus == status
+                            select r;
+                return query.ToList<Rule>();
             }
         }
-        */
         
+
+        //Method for retrieving a single Rule
+        public Rule GetRuleById(int id)
+        {
+            using (var db = new ProduceStoreEntities())
+            {
+                Rule rule = db.Rules.Find(id);
+                return rule;
+            }
+        }
+
+        //Editor-only methods
+
+        //Method for adding a rule
+        public void Add(Rule rule)
+        {
+            using (var db = new ProduceStoreEntities())
+            {
+                db.Rules.Add(rule);
+                db.SaveChanges();
+            }
+        }
+
+        //Method for editing a rule
+        public void Update(Rule rule)
+        {
+            using (var db = new ProduceStoreEntities())
+            {
+                db.Entry(rule).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+
+        //Method for deleting a rule
+        public void Delete(Rule rule)
+        {
+            using (var db = new ProduceStoreEntities())
+            {
+                db.Rules.Remove(rule);
+                db.SaveChanges();
+            }
+        }
     }
 }
