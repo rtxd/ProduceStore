06669a8611f16407da3203434e66acde8878302c
Ashna <12719213@student.uts.edu.au>
2018-06-07 18:10:06 +1000

    ProduceService.cs updated

--- UTS.ProduceStore.DomainLogic/ProduceService.cs
+++ UTS.ProduceStore.DomainLogic/ProduceService.cs
@@ -10,31 +10,51 @@ namespace UTS.ProduceStore.DomainLogic
     
     public class ProduceService
     {
+
         //Domain code for Data Maintainer 
-        
+
         //Method for retreiving all produce
-        /*public List<Produce> AllProduce()
+        public List<Produce> GetProduces(string name)
         {
+            using (var db = new ProduceStoreEntities())
+            {
+                var query = from p in db.Produces
+                            where p.ProduceName == name
+                            select p;
+                return query.ToList<Produce>();
+            }
+        }
 
-        }*/
-        
         //Method for finding produce by ID
-        /*public Produce GetProduceById(int id)
+        public Produce GetProduceById(int id)
         {
+            using (var db = new ProduceStoreEntities())
+            {
+                Produce Find = db.Produces.Find(id);
+                return Find;
+            }
+            
+        }
 
-        }*/
-        
         //Method for adding produce
-        /*public void Add(Produce produce)
+        public void Add(Produce produce)
         {
-
-        }*/
+            using (var db = new ProduceStoreEntities())
+            {
+                db.Produces.Add(produce);
+                db.SaveChanges();
+            }
+        }
 
         //Method for editing produce
-        /*public void Update(Produce produce)
+        public void Update(Produce produce)
         {
-
-        }*/
+            using (var db = new ProduceStoreEntities())
+            {
+                db.Entry(produce).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
 
         //Method for deleting product
         /*public void Delete()
